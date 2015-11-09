1f49eed25660e616449364035e9d7ab9e081faf1
HoffsMH <hecker.matthew@gmail.com>
2015-11-09 10:40:40 -0700

    added basic fetch post functionality

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,24 +1,34 @@
 $(document).ready(function () {
-  $.ajax({
+
+  var fetch_post =  function () {$.ajax({
     type: 'GET',
     url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
     success: function(posts) {
-
+      $('#latest-posts').html('')
       $.each(posts, function(index, post) {
         $('#latest-posts').append(
           "<div class='post' data-id='"+post.id+"''><h6>Published on "+ post.created_at+" </h6><p> "+post.description+" </p></div>"
         )
       })
     }
-  });
+  });}
+  $('button[name=button-fetch]').on('click',  function () {
+    fetch_post();
+  })
+  fetch_post();
+
   $('#create-post').on('click',  function () {
-    var postDescription = $('post-description').val
+    var postDescription = $('#post-description').val()
     var postParams  = { post: {description: postDescription}}
     $.ajax({
       type: 'POST',
       url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
-      data: postParams
-      success: function() { console.log('winter is coming') }
+      data: postParams,
+      success: function(posts) {
+        thing1();
+        $('#post-description').val('')
+
+      }
     });
   })
 });
