05b382ca454d49f133f620f3318828bd4f099e16
HoffsMH <hecker.matthew@gmail.com>
2015-11-09 10:08:21 -0700

    starting post button

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -2,10 +2,22 @@ $(document).ready(function () {
   $.ajax({
     type: 'GET',
     url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
-    success: function(post) {
-      $('#latest-posts').append(
-        "<div class='post'><h6>Published on some date</h6><p></p></div>"
-      )
+    success: function(posts) {
+
+      $.each(posts, function(index, post) {
+        $('#latest-posts').append(
+          "<div class='post' data-id='"+post.id+"''><h6>Published on "+ post.created_at+" </h6><p> "+post.description+" </p></div>"
+        )
+      })
     }
   });
+  $('#create-post').on('click',  function () {
+    var description = $('post-description').val
+    $.ajax({
+      type: 'POST',
+      url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+      parameters: description
+      success: function() { console.log('winter is coming') }
+    });
+  })
 });
