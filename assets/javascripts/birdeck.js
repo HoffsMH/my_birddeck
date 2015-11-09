cf0f0332ab14f10e361a9a72a20bcd1714c20aa0
HoffsMH <hecker.matthew@gmail.com>
2015-11-09 09:37:54 -0700

    basic scaffolding for post

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   $.ajax({
     type: 'GET',
     url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
-    success: function() {
-      console.log("success")
+    success: function(post) {
+      $('#latest-posts').append(
+        "<div class='post'><h6>Published on some date</h6><p></p></div>"
+      )
     }
   });
 });
